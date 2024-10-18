9f78b70e6dfaff06d3d913c74e8232e4dfe4da59
lammoi <phungnlam2003@gmaol.com>
2024-10-18 15:34:43 +0700

    add file html, css, js

---
+++ ThucTapCS/admin/xoa_nv.php
@@ -0,0 +1,7 @@
+<?php
+    include('/ThucTapCS/connection/connection.php');
+    $id_nv = $_GET['id'];
+    $sql = "DELETE from nhanvien where id_nv = '".$id_nv."'";
+    $con->query($sql);
+    header("Location: danhsach_nv.php");
+?>
